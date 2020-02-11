ae913d880ab8b68519c41da6f5c7227c58d89258
Filip Kotenko <39651273+Kotfil@users.noreply.github.com>
2020-02-11 22:55:54 +0200

    11(1)

--- theory/src/App.js
+++ theory/src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Hello World</h1>
-    </div>
-  );
-}
+class App extends Component {
+  render() {
 
+  // return (
+    // <div className="App">
+    //  <h1>Hello World</h1>
+    // </div>
+    return React.createElement(
+      'div',
+      null,
+      'h1',   
+      'Hello world!'
+    )
+  }
+}
 export default App;

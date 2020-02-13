0c5dadc7a1187096e87707e0a6159c11e9533939
FIlipp <filippkotenko@outlook.com>
2020-02-14 01:40:39 +0200

    14mi

--- theory/src/App.js
+++ theory/src/App.js
@@ -3,16 +3,32 @@ import "./App.css";
 import Car from "./Car/Car";
 
 class App extends Component {
+
+  state = {
+    cars: [
+      {name: 'Ford', year: 2010},
+      {name: 'Vaz', year: 1998},
+      {name: 'Skoda', year: 2007},
+      {name: 'Lanos', year: 2007},
+      {name: 'Mercedez', year: 2016}
+    ],
+  }
+
   render() {
     const divStyle = {
       textAlign: "center"
-    };
+    }; 
+
+    const cars = this.state.cars
+
     return (
       <div style={divStyle}>
         <h1>Hello World</h1>
-        <Car />
-        <Car />
-        
+        <Car name={cars[0].name} year={cars[0].year}/>
+        <Car name={cars[1].name} year={cars[1].year}/>
+        <Car name={cars[2].name} year={cars[2].year}/>
+        <Car name={cars[3].name} year={cars[3].year}/>
+        <Car name={cars[4].name} year={cars[4].year}/>
       </div>
     );
   }

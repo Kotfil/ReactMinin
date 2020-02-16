3d9227f33473b55ed89f02ff3a3ba140b4d8a185
Filip Kotenko <39651273+Kotfil@users.noreply.github.com>
2020-02-16 14:54:24 +0200

    16min

--- theory/src/App.js
+++ theory/src/App.js
@@ -12,35 +12,52 @@ class App extends Component {
       {name: 'Lanos', year: 2007},
       {name: 'Mercedez', year: 2016}
     ],
-    pageTitle: 'React components'
+    pageTitle: 'React components',
+    showCars: false
   }
 
-  changeTitleHandler = () => {
-      console.log('ClickOn')
-  };
+  toggleCarsGandler = () => {
+        this.setState({
+            showCars: !this.state.showCars
+        })
 
+  }
+
+  onChangeName(name, index) {
+    console.log(name,index)
+  }
+ 
   render() {
+      console.log('Render')
     const divStyle = {
       textAlign: "center"
     }; 
 
-    const cars = this.state.cars
+
 
     return (
       <div style={divStyle}>
         <h1>{this.state.pageTitle}</h1>
 
-        <button onClick={this.changeTitleHandler}>Change title</button>
+        <button
+         onClick={this.toggleCarsGandler}>
+             Toggle cars</button>
 
-
-        <Car name={cars[0].name} year={cars[0].year}/>
-        <Car name={cars[1].name} year={cars[1].year}/>
-        <Car name={cars[2].name} year={cars[2].year}/>
-        <Car name={cars[3].name} year={cars[3].year}/>
-        <Car name={cars[4].name} year={cars[4].year}/>
+        { this.state.showCars 
+          ?  this.state.cars.map((car, index) => {
+            return (
+                <Car 
+                    key={index}
+                    name={car.name}
+                    year={car.year}
+                    onChangeName={event => this.onChangeName(event.taget.value,index)}
+                />
+            )
+        })
+        : null
+    }
       </div>
-    );
+        );
   }
-}
-
+  }
 export default App;
--- theory/src/Car/Car.js
+++ theory/src/Car/Car.js
@@ -1,8 +1,13 @@
 import React from "react";
 
 export default props => (
-  <div>
+  <div style={{
+      border: '1px solid #ccc',
+      marginBottom: '10px'
+  }}>
     <h3>Car name: {props.name}</h3>
     <p>Year: <strong>{props.year}</strong></p>
+    <input type="text" onChange={props.onChangeName}/>
+    {/* <button onClick={props.onChangeTitle}>Click</button> */}
   </div>
 );

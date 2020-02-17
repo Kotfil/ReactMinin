8c982f3e2f2346d2658975a96d37112b9eaf3187
FIlipp <filippkotenko@outlook.com>
2020-02-17 18:54:57 +0200

    17min

--- theory/src/App.js
+++ theory/src/App.js
@@ -3,15 +3,19 @@ import './App.css';
 import Car from './Car/Car'
 
 class App extends Component {
-
-  state = {
-    cars: [
-      {name: 'Ford', year: 2018},
-      {name: 'Audi', year: 2016},
-      {name: 'Mazda', year: 2010}
-    ],
-    pageTitle: 'React components',
-    showCars: false
+  
+  constructor(props) {
+    console.log('App constructor')
+    super(props)
+    this.state = {
+      cars: [
+        {name: 'Ford', year: 2018},
+        // {name: 'Audi', year: 2016},
+        // {name: 'Mazda', year: 2010}
+      ],
+      pageTitle: 'React components',
+      showCars: false
+    }
   }
 
   toggleCarsHandler = () => {
@@ -36,7 +40,15 @@ class App extends Component {
 
   }
 
+    componentWillMount() {
+      console.log('App componentWillMount')
+    }
+    componentDidMount() {
+      console.log('App componentDidMount')
+    }
+
   render() {
+    console.log('App render')
     const divStyle = {
       textAlign: 'center'
     }
--- theory/src/Car/Car.js
+++ theory/src/Car/Car.js
@@ -1,17 +1,64 @@
 import React from 'react'
+import './Car.css'
+import Radium from 'radium'
 
-export default props => (
-  <div style={{
+class Car extends React.Component {
+
+  componentWillReceiveProps(nextProps) {
+    console.log('Car componentWillReceiveProps', nextProps)
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('Car shouldComponentUpdate',nextState, nextProps)
+    return true
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log('Car componentWillUpdate', nextProps,nextState)
+  }
+
+  componentDidUpdate() {
+    console.log('Car componentDidUpdate',)
+  }
+
+  render() {
+    console.log('Car render')
+    const inputClasses = [classes.input]
+  const inputClasses = ['input']
+
+  if (this.props.name !== '') {
+    inputClasses.push('green')
+  } else {
+    inputClasses.push('red')
+  }
+
+  if (this.props.name.length >4) {
+    inputClasses.push('bold')
+  }
+  const style = {
     border: '1px solid #ccc',
-    marginBottom: '10px',
-    display: 'block',
-    padding: '10px',
-          boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .14)',
-          borderRadius: '5px'
-  }}>
-    <h3>Сar name: {props.name}</h3>
-    <p>Year: <strong>{props.year}</strong></p>
-    <input type="text" onChange={props.onChangeName} value={props.name} />
-    <button onClick={props.onDelete}>Delete</button>
+    boxShadow: '0 4px 5px 0 rgba(0,0,0, .14)',
+    ':hover':{
+      border:'1px solid #aaa',
+      boxShadow: '0 4px 15px 0 rgba( 0,0,0, .25)',
+      cursor: 'pointer'
+    }
+  }
+  
+  return (  <div className="Car" style={style}>
+  <h3>Сar name: {this.props.name}</h3>
+  <p>Year: <strong>{this.props.year}</strong></p>
+  <input 
+  type="text" 
+  onChange={this.props.onChangeName} 
+  value={this.props.name} 
+  className={inputClasses.join(' ')}
+  />
+  <button onClick={this.props.onDelete}>Delete</button>
   </div>
-)
+  )
+
+  }
+}
+
+export default Car;

61ac2775f1b57adf44da031b8609ca8201be09f6
FilippK <filippkotenko@outlook.com>
2020-02-25 12:34:29 +0200

    25min(2)

--- quiz/src/hoc/Layout/Layout.js
+++ quiz/src/hoc/Layout/Layout.js
@@ -1,16 +1,16 @@
 import React, {Component} from 'react'
+import classes from './Layout.css'
 
 class Layout extends Component {
-    render() {
-        return (
-            <div>
-
-                <main>
-                {this.props.children}
-                </main>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className={classes.Layout}>
+        <main>
+          { this.props.children }
+        </main>
+      </div>
+    )
+  }
 }
 
 export default Layout

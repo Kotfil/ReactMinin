e98cd0c196c0fc4bae49dfa25468b0cc87f1ebd0
FIlipp <filippkotenko@outlook.com>
2020-05-20 18:45:52 +0300

    base repeat

---
+++ Base/02_numbers.js
@@ -0,0 +1,35 @@
+// console.log(0.4 + 0.2) //0.6
+// console.log(2/5 + (1 / 5)) //0.6
+// console.log(parseFloat((0.4 + 0.2).toFixed(1))) //0.6
+// BigInt
+
+// console.log(9007199254740991n - 90071992547409)
+// console.log(10n - 4) - error
+// console.log(parseInt(10n) - BigInt(4))
+
+//3 Math
+// console.log(Math.E)
+// console.log(Math.PI)
+//
+// console.log(Math.sqrt(25))
+// console.log(Math.pow(5,3))
+// console.log(Math.abs(-42))
+// console.log(Math.max(42,12,23,11,422))
+// console.log(Math.min(42,12,23,11,422))
+// console.log(Math.ceil(4.6))
+// console.log(Math.round(4.9))
+// console.log(Math.trunc(4.9))
+// console.log(Math.random())
+
+//4 Example
+
+// function getRandomBetween(min,max) {
+//     return Math.floor(Math.random() * (max - min + 1) + min)
+//
+// }
+//
+// console.log(getRandomBetween(10, 42));
+
+
+
+
---
+++ Base/03_string.js
@@ -0,0 +1,45 @@
+// // const name = 'Filipp'
+// // const age = 27
+// //
+// // function getAge() {
+// //     return age
+// // }
+//
+// //const output = 'Привет меня зову ' + name + ' и мой возраст ' + age + ' лет';
+// //const output = `Привет. меня зовут ${name} и мой возраст ${age > 20? 'A' : 'B'} лет`
+// //console.log(output)
+//
+// // const output = `
+// // <div> This is div</div>
+// // <p>this is o</p>`
+// // const name = 'Filipp'
+// // //new String()
+// // console.log(name.length)
+// // console.log(name.toUpperCase())
+// // console.log(name.toLowerCase())
+// // console.log(name.charAt(2))
+// // console.log(name.indexOf('q'))
+// // console.log(name.startsWith('Fil'))
+// // console.log(name.endsWith('pp'))
+// // console.log(name.toLowerCase().startsWith('fil'))
+// // console.log(name.repeat(3))
+// //
+// // const string =  '              password'
+// // console.log(string)
+// // console.log(string.trim())
+//
+// function logPerson(s, name, age) {
+//     if (age < 0) {
+//          age = 'Еще не родился'
+//     }
+//     console.log(s,name,age)
+//     return `${s[0]},${name}${s[1]}${age}${s[2]}`
+// }
+// const personName = 'Филипп'
+// const personName2 = 'Максим'
+// const personeAge = 27
+// const personeAge2 = -10
+// const output = logPerson`Имя: ${personName}, Возраст: ${personeAge}!`
+// const output2 = logPerson`Имя: ${personName2}, Возраст: ${personeAge2}!`
+// console.log(output)
+// console.log(output2)

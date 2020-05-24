6ef1e833d9f293d1cbb429de7fd11e880d1879ee
FIlipp <filippkotenko@outlook.com>
2020-05-24 08:21:37 +0300

    Object base 5

--- Base/app.js
+++ Base/app.js
@@ -1,29 +1,61 @@
-const person = {
-    name: 'Filipp',
-    age: undefined,
-    isProgrammer: true,
-    language: ['ru', 'en', 'de'],
-    // 'complex key': 'Complex Key',
-    greet() {
-    console.log('greet fo person')},
-    info() {
-
-        console.log('this:', this)
-        console.log('Информация про человека:', this.name)
-    }
-}
-person.info()
+// const person = {
+//     name: 'Filipp',
+//     age: undefined,
+//     isProgrammer: true,
+//     language: ['ru', 'en', 'de'],
+//     // 'complex key': 'Complex Key',
+//     greet() {
+//     console.log('greet fo person')},
+//     info() {
+//
+//         console.log('this:', this)
+//         console.log('Информация про человека:', this.name)
+//     }
+// }
+//person.info()
 // const keys = Object.keys(person)
 // keys.forEach((key) => {
 //     console.log()
 // })
 
 //context
-const logger = {
-    keys() {
-        console.log(Object.keys(this))
+// const logger = {
+//     keys() {
+//         console.log(Object.keys(this))
+//     }
+// }
+
+// logger.keys(person)
+// console.log()
+
+const person = {
+    name: 'Filipp',
+    age: 27,
+    isProgrammer: true,
+    languages: ['ru', 'en', 'de'],
+    'complex key': 'Complex Value',
+    ['key_' + (1 + 3)]: 'Computed Key',
+    greet() {
+        console.log('greet fo person')
     }
 }
+// console.log(person.name)
+// const ageKey = 'age'
+// console.log(person[ageKey])
+// console.log(person['complex key'])
+// person.greet()
+//
+// person.age++
+// person.language.push('ivrit')
+// console.log(person)
+//
+// person['key_4'] = undefined
+// delete person['key_4']
+// console.log(person)
 
-logger.keys(person)
-console.log()
+// const name = person.name
+// const age = person.age
+// const languaes = person.languages
+const {name,age,languaes} = person
+
+console.log(name,age,languaes)

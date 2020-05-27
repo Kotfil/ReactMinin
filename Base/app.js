6dd2da0976bae069c74001277f24354468729b46
FIlipp <filippkotenko@outlook.com>
2020-05-27 12:27:29 +0300

    DOM

--- Base/app.js
+++ Base/app.js
@@ -7,23 +7,48 @@ const header2 = document.querySelector('h2')
 //const header3 = header2.nextElementSibling
 
 const h2list = document.querySelectorAll('h2')
-console.dir(h2list)
+//console.dir(h2list)
 const heading3 = h2list[h2list.length - 1]
 
 setTimeout(() => {
-    addStylesTo(header,'123','white')
+    addStylesTo(header,'123')
 }, 1000)
 setTimeout(() => {
-    addStylesTo(header2,'321','gray')
+    addStylesTo(header2.children[0],'321','gray')
 }, 2000)
 setTimeout(() => {
-    addStylesTo(heading3,'333','red')
+    addStylesTo(heading3,'333','red', '2rem')
 }, 3000)
 
-function addStylesTo(node, text, color = 'blue') {
+function addStylesTo(node, text, color = 'blue',fontSize) {
     node.textContent = text
     node.style.textAlign = 'center'
     node.style.color = color
     node.style.backgroundColor = 'yellow'
     node.style.padding = '2rem'
+    if (fontSize) {
+        node.style.fontSize = fontSize
+    }
 }
+
+header.onclick = () => {
+   if(header.style.color === 'red') {
+       header.style.color = '#000'
+       header.style.backgroundColor = '#fff'
+   } else {
+       header.style.color = 'red'
+       header.style.backgroundColor = '#000'
+   }
+}
+
+
+
+header2.addEventListener('dbclick', () =>  {
+    if(header2.style.color === 'yellow') {
+        header2.style.color = '#000'
+        header2.style.backgroundColor = '#fff'
+    } else {
+        header2.style.color = 'white'
+        header2.style.backgroundColor = '#000'
+    }
+})
